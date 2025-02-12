b192dc0d558409c1f177610bfd61af0c4a599425
ramro109626 <39096045-ramro109626@users.noreply.replit.com>
2025-02-12 01:05:29 +0000

    User checkpoint: Move Card class to separate file.

---
+++ Card.java
@@ -0,0 +1,35 @@
+public class Card {
+    private String suit;
+    private String name;
+    private int value;
+    private String picture;
+
+    public Card(String suit, String name, int value, String picture) {
+        this.suit = suit;
+        this.name = name;
+        this.value = value;
+        this.picture = picture;
+    }
+
+    public String getSuit() { return suit; }
+    public void setSuit(String suit) { this.suit = suit; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getValue() { return value; }
+    public void setValue(int value) { this.value = value; }
+    public String getPicture() { return picture; }
+    public void setPicture(String picture) { this.picture = picture; }
+
+    @Override
+    public String toString() {
+        return "Card{suit='" + suit + "', name='" + name + "', value=" + value + ", picture='" + picture + "'}";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Card card = (Card) obj;
+        return value == card.value;
+    }
+}
--- CardGame.java
+++ CardGame.java
@@ -5,42 +5,6 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Card {
-    private String suit;
-    private String name;
-    private int value;
-    private String picture;
-
-    public Card(String suit, String name, int value, String picture) {
-        this.suit = suit;
-        this.name = name;
-        this.value = value;
-        this.picture = picture;
-    }
-
-    public String getSuit() { return suit; }
-    public void setSuit(String suit) { this.suit = suit; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public int getValue() { return value; }
-    public void setValue(int value) { this.value = value; }
-    public String getPicture() { return picture; }
-    public void setPicture(String picture) { this.picture = picture; }
-
-    @Override
-    public String toString() {
-        return "Card{suit='" + suit + "', name='" + name + "', value=" + value + ", picture='" + picture + "'}";
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Card card = (Card) obj;
-        return value == card.value;
-    }
-}
-
 class CardGame {
     private static ArrayList<Card> deckOfCards = new ArrayList<>();
     private static ArrayList<Card> playerCards = new ArrayList<>();
